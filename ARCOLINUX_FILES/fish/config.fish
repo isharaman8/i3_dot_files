233c9f5b52d3dc8688f5f1e11a76db8830875b47
Aman Kumar <isharaman8@gmail.com>
2023-06-22 00:35:04 +0530

    added new arcolinux config files

---
+++ ARCOLINUX_FILES/fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    neofetch
+
+    # ABBREVATIONS file path
+    source /home/amankumar/.config/fish/completions/abbr.fish
+
+end
